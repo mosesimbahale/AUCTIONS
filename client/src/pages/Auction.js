4dccbd10a46b92dae7f699382652cea9d75c402c
moses imbahale <42868535+mosesimbahale@users.noreply.github.com>
2022-03-27 00:13:10 +0300

    Update Auction.js

--- client/src/pages/Auction.js
+++ client/src/pages/Auction.js
@@ -6,6 +6,214 @@ const Auction = () => {
    
    
    
+
+    <div className="container px-2 mx-auto">
+    <div className="flex flex-wrap">
+      <div className="flex flex-wrap items-center justify-between -mx-4 align-baseline">
+
+
+
+<div class="md:flex shadow-sm  mx-6 md:mx-auto  max-w-lg md:max-w-7xl h-64   px-3 mt-2">
+  <img
+    class="h-full w-full md:w-1/4  object-cover rounded-l-lg pb-5/6"
+    src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg"
+    alt="bag"
+  />
+
+  <div class="w-full md:w-1/2 px-4 py-4 bg-white rounded-sm">
+    <div class="flex items-center">
+      <h2 class="text-xl text-gray-800 font-medium mr-auto">
+        Your Travel Buddy
+      </h2>
+      <p class="text-gray-800 font-semibold tracking-tighter">
+        only
+        <i class="text-gray-600 line-through">60$</i>
+        Amount $$$
+      </p>
+    </div>
+    <p class="text-sm text-gray-700 mt-4 h-20">
+      Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam
+      reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit
+      earum at sequ adipisicing elit.
+    </p>
+
+    <div class="flex items-center justify-end mt-4 top-auto">
+      <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+        Dimension
+      </button>
+      <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+        Timer Count
+      </button>
+      <button class=" bg-indigo-600 text-gray-200 px-2 py-2 rounded-md ">
+        Active
+      </button>
+    </div>
+
+    <div class="flex items-center justify-end mt-4 top-auto ">
+      <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+        {" "}
+        ⭐⭐⭐⭐⭐
+      </button>
+      <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+        Productanalysis
+      </button>
+      <button class=" bg-purple-600 text-gray-200 px-2 py-2 rounded-md ">
+        RaiseBid
+      </button>
+    </div>
+  </div>
+
+  <div className="h-full w-full md:w-1/4  object-cover  pb-5/6 bg-white md:flex shadow-sm   md:mx-auto  max-w-lg md:max-w-2xl  border-solid border-2 border-indigo-500 rounded-lg p-2">
+    {" "}
+    CHART
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="md:flex shadow-sm  mx-6 md:mx-auto  max-w-lg md:max-w-7xl h-64   px-3 mt-2">
+  <img
+    class="h-full w-full md:w-1/4  object-cover rounded-l-lg pb-5/6"
+    src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg"
+    alt="bag"
+  />
+
+  <div class="w-full md:w-1/2 px-4 py-4 bg-white rounded-sm">
+    <div class="flex items-center">
+      <h2 class="text-xl text-gray-800 font-medium mr-auto">
+        Your Travel Buddy
+      </h2>
+      <p class="text-gray-800 font-semibold tracking-tighter">
+        only
+        <i class="text-gray-600 line-through">60$</i>
+        Amount $$$
+      </p>
+    </div>
+    <p class="text-sm text-gray-700 mt-4 h-20">
+      Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam
+      reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit
+      earum at sequ adipisicing elit.
+    </p>
+
+    <div class="flex items-center justify-end mt-4 top-auto">
+      <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+        Dimension
+      </button>
+      <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+        Timer Count
+      </button>
+      <button class=" bg-indigo-600 text-gray-200 px-2 py-2 rounded-md ">
+        Active
+      </button>
+    </div>
+
+    <div class="flex items-center justify-end mt-4 top-auto ">
+      <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+        {" "}
+        ⭐⭐⭐⭐⭐
+      </button>
+      <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+        Productanalysis
+      </button>
+      <button class=" bg-purple-600 text-gray-200 px-2 py-2 rounded-md ">
+        RaiseBid
+      </button>
+    </div>
+  </div>
+
+  <div className="h-full w-full md:w-1/4  object-cover  pb-5/6 bg-white md:flex shadow-sm   md:mx-auto  max-w-lg md:max-w-2xl  border-solid border-2 border-indigo-500 rounded-lg p-2">
+    {" "}
+    CHART
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="md:flex shadow-sm  mx-6 md:mx-auto  max-w-lg md:max-w-7xl h-64   px-3 mt-2">
+  <img
+    class="h-full w-full md:w-1/4  object-cover rounded-l-lg pb-5/6"
+    src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg"
+    alt="bag"
+  />
+
+  <div class="w-full md:w-1/2 px-4 py-4 bg-white rounded-sm">
+    <div class="flex items-center">
+      <h2 class="text-xl text-gray-800 font-medium mr-auto">
+        Your Travel Buddy
+      </h2>
+      <p class="text-gray-800 font-semibold tracking-tighter">
+        only
+        <i class="text-gray-600 line-through">60$</i>
+        Amount $$$
+      </p>
+    </div>
+    <p class="text-sm text-gray-700 mt-4 h-20">
+      Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam
+      reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit
+      earum at sequ adipisicing elit.
+    </p>
+
+    <div class="flex items-center justify-end mt-4 top-auto">
+      <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+        Dimension
+      </button>
+      <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+        Timer Count
+      </button>
+      <button class=" bg-indigo-600 text-gray-200 px-2 py-2 rounded-md ">
+        Active
+      </button>
+    </div>
+
+    <div class="flex items-center justify-end mt-4 top-auto ">
+      <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+        {" "}
+        ⭐⭐⭐⭐⭐
+      </button>
+      <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+        Productanalysis
+      </button>
+      <button class=" bg-purple-600 text-gray-200 px-2 py-2 rounded-md ">
+        RaiseBid
+      </button>
+    </div>
+  </div>
+
+  <div className="h-full w-full md:w-1/4  object-cover  pb-5/6 bg-white md:flex shadow-sm   md:mx-auto  max-w-lg md:max-w-2xl  border-solid border-2 border-indigo-500 rounded-lg p-2">
+    {" "}
+    CHART
+  </div>
+</div>
+
+
+
+
+
+
+
+
+</div>
+</div>
+</div>
+
     
 
 
