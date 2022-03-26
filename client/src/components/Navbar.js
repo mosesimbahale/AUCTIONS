df7917b5b3e369a669f22d03a69d54843801324d
moses imbahale <42868535+mosesimbahale@users.noreply.github.com>
2022-03-27 00:09:58 +0300

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -17,10 +17,23 @@ const Sidebar = () => {
                 </p>
               </div>
 
+
+              <div className="w-full md:w-auto px-4 mb-14 md:mb-0">
+                <h2 className="text-2xl md:text-3xl font-heading font-bold leading-relaxed">
+                  Product analysis
+                </h2>
+
+                <p className="text-darkBlueGray-400 leading-8">
+                  Achieve your goals and increase profits
+                </p>
+              </div>
+
+              
+
               <div className="w-full md:w-auto px-4">
                 <div className="flex items-center">
                   <a
-                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
+                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-indigo-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
                     href="/signin"
                   >
                     All
@@ -34,14 +47,14 @@ const Sidebar = () => {
                   </a>
 
                   <a
-                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
+                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-orange-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
                     href="/signin"
                   >
                     All
                   </a>
 
                   <a
-                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
+                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-purple-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
                     href="/signin"
                   >
                     All
