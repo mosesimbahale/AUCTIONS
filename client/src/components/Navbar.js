fab63e891194770b1ff37a44503eadd8d983d43e
moses imbahale <42868535+mosesimbahale@users.noreply.github.com>
2022-03-27 00:05:55 +0300

    update

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <section className="py-2 bg-blueGray-100">
+        <div className="container px-2 mx-auto">
+          <div className="flex flex-wrap">
+            <div className="flex flex-wrap items-center justify-between -mx-4">
+              <div className="w-full md:w-auto px-4 mb-14 md:mb-0">
+                <h2 className="text-2xl md:text-3xl font-heading font-bold leading-relaxed">
+                  Product analysis
+                </h2>
+
+                <p className="text-darkBlueGray-400 leading-8">
+                  Achieve your goals and increase profits
+                </p>
+              </div>
+
+              <div className="w-full md:w-auto px-4">
+                <div className="flex items-center">
+                  <a
+                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
+                    href="/signin"
+                  >
+                    All
+                  </a>
+
+                  <a
+                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
+                    href="/signin"
+                  >
+                    All
+                  </a>
+
+                  <a
+                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
+                    href="/signin"
+                  >
+                    All
+                  </a>
+
+                  <a
+                    className="inline-block py-2 px-6 m-4 text-sm leading-6 text-white font-medium tracking-tighter font-heading text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl"
+                    href="/signin"
+                  >
+                    All
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,324 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <div className="App">
+        <Navbar />
+
+        <header className="App-header">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </header>
+      </div>
+
+
+
+
+
+
+
+
+{/*
+      <div className="flex-row mt-4">
+        <div className="grid grid-cols-1 divide-x-{} p-4 m-2 bg-white">
+          <table className="table-fixed">
+            <thead>
+              <tr>
+                <th className="w-3/4 px-4 py-2">Products</th>
+                <th className="w-1/4 px-4 py-2">Location</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td className="border px-4 ">
+                  <div className="grid grid-cols-3 divide-x">
+                    <div> mmm</div>
+
+                    <div>image02</div>
+                    <div>image03</div>
+                  </div>
+                </td>
+                <td className="border px-4 py-2">Map</td>
+              </tr>
+
+              <tr className="bg-blue-100">
+                <td className="border px-4 py-2">
+                  <div className="grid grid-cols-3 divide-x">
+                    <div>moreinfo101</div>
+                    <div>moreinfo02</div>
+                    <div>moreinfo03</div>
+                  </div>
+                </td>
+                <td className="border px-4 py-2">Visualizer</td>
+              </tr>
+
+              <tr>
+                <td className="border px-4 py-2">
+                  <div className="grid grid-cols-3 divide-x">
+                    <div>buttons01</div>
+                    <div>buttons02</div>
+                    <div>buttons03</div>
+                  </div>
+                </td>
+                <td className="border px-4 py-2">Butons2</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+
+
+  */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div className="container px-2 mx-auto">
+          <div className="flex flex-wrap">
+            <div className="flex flex-wrap items-center justify-between -mx-4 align-baseline">
+
+
+
+      <div class="md:flex shadow-sm  mx-6 md:mx-auto  max-w-lg md:max-w-7xl h-64   px-3 mt-2">
+        <img
+          class="h-full w-full md:w-1/4  object-cover rounded-l-lg pb-5/6"
+          src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg"
+          alt="bag"
+        />
+
+        <div class="w-full md:w-1/2 px-4 py-4 bg-white rounded-sm">
+          <div class="flex items-center">
+            <h2 class="text-xl text-gray-800 font-medium mr-auto">
+              Your Travel Buddy
+            </h2>
+            <p class="text-gray-800 font-semibold tracking-tighter">
+              only
+              <i class="text-gray-600 line-through">60$</i>
+              Amount $$$
+            </p>
+          </div>
+          <p class="text-sm text-gray-700 mt-4 h-20">
+            Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam
+            reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit
+            earum at sequ adipisicing elit.
+          </p>
+
+          <div class="flex items-center justify-end mt-4 top-auto">
+            <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+              Dimension
+            </button>
+            <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+              Timer Count
+            </button>
+            <button class=" bg-indigo-600 text-gray-200 px-2 py-2 rounded-md ">
+              Active
+            </button>
+          </div>
+
+          <div class="flex items-center justify-end mt-4 top-auto ">
+            <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+              {" "}
+              ⭐⭐⭐⭐⭐
+            </button>
+            <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+              Productanalysis
+            </button>
+            <button class=" bg-purple-600 text-gray-200 px-2 py-2 rounded-md ">
+              RaiseBid
+            </button>
+          </div>
+        </div>
+
+        <div className="h-full w-full md:w-1/4  object-cover  pb-5/6 bg-white md:flex shadow-sm   md:mx-auto  max-w-lg md:max-w-2xl  border-solid border-2 border-indigo-500 rounded-lg p-2">
+          {" "}
+          CHART
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+      <div class="md:flex shadow-sm  mx-6 md:mx-auto  max-w-lg md:max-w-7xl h-64   px-3 mt-2">
+        <img
+          class="h-full w-full md:w-1/4  object-cover rounded-l-lg pb-5/6"
+          src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg"
+          alt="bag"
+        />
+
+        <div class="w-full md:w-1/2 px-4 py-4 bg-white rounded-sm">
+          <div class="flex items-center">
+            <h2 class="text-xl text-gray-800 font-medium mr-auto">
+              Your Travel Buddy
+            </h2>
+            <p class="text-gray-800 font-semibold tracking-tighter">
+              only
+              <i class="text-gray-600 line-through">60$</i>
+              Amount $$$
+            </p>
+          </div>
+          <p class="text-sm text-gray-700 mt-4 h-20">
+            Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam
+            reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit
+            earum at sequ adipisicing elit.
+          </p>
+
+          <div class="flex items-center justify-end mt-4 top-auto">
+            <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+              Dimension
+            </button>
+            <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+              Timer Count
+            </button>
+            <button class=" bg-indigo-600 text-gray-200 px-2 py-2 rounded-md ">
+              Active
+            </button>
+          </div>
+
+          <div class="flex items-center justify-end mt-4 top-auto ">
+            <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+              {" "}
+              ⭐⭐⭐⭐⭐
+            </button>
+            <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+              Productanalysis
+            </button>
+            <button class=" bg-purple-600 text-gray-200 px-2 py-2 rounded-md ">
+              RaiseBid
+            </button>
+          </div>
+        </div>
+
+        <div className="h-full w-full md:w-1/4  object-cover  pb-5/6 bg-white md:flex shadow-sm   md:mx-auto  max-w-lg md:max-w-2xl  border-solid border-2 border-indigo-500 rounded-lg p-2">
+          {" "}
+          CHART
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+      <div class="md:flex shadow-sm  mx-6 md:mx-auto  max-w-lg md:max-w-7xl h-64   px-3 mt-2">
+        <img
+          class="h-full w-full md:w-1/4  object-cover rounded-l-lg pb-5/6"
+          src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg"
+          alt="bag"
+        />
+
+        <div class="w-full md:w-1/2 px-4 py-4 bg-white rounded-sm">
+          <div class="flex items-center">
+            <h2 class="text-xl text-gray-800 font-medium mr-auto">
+              Your Travel Buddy
+            </h2>
+            <p class="text-gray-800 font-semibold tracking-tighter">
+              only
+              <i class="text-gray-600 line-through">60$</i>
+              Amount $$$
+            </p>
+          </div>
+          <p class="text-sm text-gray-700 mt-4 h-20">
+            Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam
+            reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit
+            earum at sequ adipisicing elit.
+          </p>
+
+          <div class="flex items-center justify-end mt-4 top-auto">
+            <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+              Dimension
+            </button>
+            <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+              Timer Count
+            </button>
+            <button class=" bg-indigo-600 text-gray-200 px-2 py-2 rounded-md ">
+              Active
+            </button>
+          </div>
+
+          <div class="flex items-center justify-end mt-4 top-auto ">
+            <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">
+              {" "}
+              ⭐⭐⭐⭐⭐
+            </button>
+            <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">
+              Productanalysis
+            </button>
+            <button class=" bg-purple-600 text-gray-200 px-2 py-2 rounded-md ">
+              RaiseBid
+            </button>
+          </div>
+        </div>
+
+        <div className="h-full w-full md:w-1/4  object-cover  pb-5/6 bg-white md:flex shadow-sm   md:mx-auto  max-w-lg md:max-w-2xl  border-solid border-2 border-indigo-500 rounded-lg p-2">
+          {" "}
+          CHART
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+</div>
+</div>
+</div>
+
+
+
+    </>
+  );
+};
+
+export default Home;
