36f9084503d8c20ccfd01d0e4c4a4eb701173ab8
moses imbahale <42868535+mosesimbahale@users.noreply.github.com>
2022-03-27 10:41:04 +0300

    updated server 1.1

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const { v4:uuid } = require('uuid');
+const { format } =require('date-fns');
+
+
+console.log(format(new Date(), 'yyyy-MM-dd HH:mm:ss'));
+
+
+console.log(uuid());
+
+
+
+
+console.log('hello world');
