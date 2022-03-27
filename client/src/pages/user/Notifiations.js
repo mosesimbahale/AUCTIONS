fb27d9c7f1c264542ea6549f67674ab7eae35953
moses imbahale <42868535+mosesimbahale@users.noreply.github.com>
2022-03-27 11:12:09 +0300

    update create notifications

---
+++ client/src/pages/user/Notifiations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notifiations = () => {
+  return (
+    <div>Notifiations</div>
+  )
+}
+
+export default Notifiations;
---
+++ client/src/pages/user/Singlepost.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Singlepost = () => {
+  return (
+    <div>Singlepost</div>
+  )
+}
+
+export default Singlepost;
